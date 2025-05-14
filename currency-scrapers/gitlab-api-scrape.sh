72a0b3dc90fe61d93e2e731274d5a7d934f1f08b
Rahul Chandra <rahul@projecteclipse.org>
2025-05-13 20:26:43 -0400

    Fix trailing slash error and cleanup

--- currency-scrapers/gitlab-api-scrape.sh
+++ currency-scrapers/gitlab-api-scrape.sh
@@ -9,9 +9,9 @@ if [[ $? -ne 0 ]]; then
 fi
 REPO=$(cat $(find . -name $PACKAGE) | grep URL | head -1 | sed 's/.*\.com//' | sed 's/.*\.org//' | sed 's|/|!|3' | sed 's/!.*//' | sed 's/^.//')
 PROJECT_ID=$(echo $REPO | sed 's!/!%2F!')
-GITLAB_SERVER=$(cat $(find . -name $PACKAGE)  | grep URL= | sed 's/URL=//' | sed 's/.com.*/.com\//g' | sed 's/.org.*/.org\//g' )
+GITLAB_SERVER=$(find . -name "$PACKAGE" -exec grep -m1 '^URL=' {} \; | cut -d'=' -f2 | sed -E 's|(https?://[^/]+).*|\1|')
 LATEST_VER=$(curl \
-  -Ls "$GITLAB_SERVER/api/v4/projects/$PROJECT_ID/releases/permalink/latest" | grep tag_name | sed 's/.$//' | sed 's/\"//g' | sed 's/.*://' | sed 's/^.//' | sed 's/.*-//g' | sed 's/v//' | sed 's!.*/!!' | sed 's/.$//' )
+  -Ls "${GITLAB_SERVER}/api/v4/projects/$PROJECT_ID/releases/permalink/latest" |  jq -r '.tag_name' | sed  -e 's/^v//' -e 's/.*-//')
 # Some apps have TAG_OVERRIDE because the tags are newer than the releases, in that case use those instead
 cat $(find . -name $PACKAGE) | grep TAG_OVERRIDE &> /dev/null
 if [[ $? == 0 ]]; then
