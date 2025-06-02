2651e57b396b8013584b77e584042892a19e45ce
Rahul Chandra <rahul@projecteclipse.org>
2025-06-01 21:32:51 -0400

    Force github-api to remove .git cleanup

--- currency-scrapers/github-api-scrape.sh
+++ currency-scrapers/github-api-scrape.sh
@@ -6,12 +6,12 @@ cat $(find . -name $PACKAGE -not -path './.git*') | grep github &> /dev/null
 if [[ $? -ne 0 ]]; then
    exit 1
 fi
-REPO=$(cat $(find . -name $PACKAGE -not -path './.git*') | grep URL | head -1 | sed 's/.*\.com//' | sed 's|/|!|3' | sed 's/!.*//' | sed 's/^.//')
+REPO=$(cat $(find . -name $PACKAGE -not -path './.git*') | grep URL | head -1 | sed -e 's/.*\.com//' -e  's/\.git//' -e 's|/|!|3' -e  's/!.*//' -e 's/^.//')
 LATEST_VER=$(curl -Ls \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_API_KEY "\
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$REPO/releases/latest | grep tag_name | sed 's/.$//' | sed 's/\"//g' | sed 's/.*://' | sed 's/^.//' | sed 's/.*-//g' | sed 's/v//')
+  https://api.github.com/repos/$REPO/releases/latest | grep tag_name | sed -e 's/.$//' -e 's/\"//g' -e 's/.*://' -e 's/^.//' -e  's/.*-//g' -e 's/v//')
 # Some apps have TAG_OVERRIDE because the tags are newer than the releases, in that case use those instead
 cat $(find . -name $PACKAGE -not -path './.git*') | grep TAG_OVERRIDE &> /dev/null
 if [[ $? == 0 ]]; then
@@ -24,6 +24,6 @@ if [[ -z $LATEST_VER ]]; then
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_API_KEY "\
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$REPO/tags | grep name | head -1 | sed 's/.$//' | sed 's/\"//g' | sed 's/.*://' | sed 's/^.//' | sed 's/.*-//g' | sed 's/v//')
+  https://api.github.com/repos/$REPO/tags | grep name | head -1 | sed -e 's/.$//' -e 's/\"//g' -e 's/.*://' -e  's/^.//' -e 's/.*-//g' -e 's/v//')
 fi
 echo $LATEST_VER
