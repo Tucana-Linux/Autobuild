bee73ac603faaebb4b012c2be7d2b1c386db985e
Rahul Chandra <rahul@chandra.net>
2025-09-25 20:09:12 -0700

    gst-plugins-rs: Pin currency to gst-plugins-bad

--- currency-scrapers/gst-plugins-rs.sh
+++ currency-scrapers/gst-plugins-rs.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 source autobuild.conf
-URL=https://gstreamer.freedesktop.org/src/gst-plugins-bad
-PACKAGE_PREFIX=$(cat $(find $BUILD_SCRIPTS_ROOT/ -name $1) | grep URL | head -1 | sed 's/URL=//g'| sed -r 's|(.*)/||'| sed 's|.tar.*||g' | sed 's|-[^-]*$||g')
+URL=https://gstreamer.freedesktop.org/src/gst-plugins-bad/
+PACKAGE_PREFIX=gst-plugins-bad
 
 VERSIONS_REPEAT=$(python3 $SCRAPER_LOCATIONS/classic-scrape.py $URL | grep $PACKAGE_PREFIX- | sed 's/.*-//g' | sed 's/.[a-z].*//g')
 VERSIONS=$(awk 'BEGIN{RS=ORS="\n"}!a[$0]++' <<< $VERSIONS_REPEAT)
 
-echo $VERSIONS | sed -e 's/\ /\n/g'  -e 's/[[:alpha:]]//g' | grep -E '[0-9]\.[0-9][0,2,4,6,8].[0-9]+' | sort -rV | head -1
-
-
+echo $VERSIONS  | sed -e 's/\ /\n/g'  -e 's/[[:alpha:]]//g' | grep -E '[0-9]\.[0-9][0,2,4,6,8].[0-9]+' | sort -rV | head -1
