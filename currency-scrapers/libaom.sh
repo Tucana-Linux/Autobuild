6de0471d3e5aa9e9eef67884fa4b875be362906b
Rahul Chandra <rahul@chandra.net>
2025-09-26 21:58:51 -0700

    Actually add libaom to currency

--- currency-scrapers/libaom.sh
+++ currency-scrapers/libaom.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 PACKAGE=$1
 wget "https://storage.googleapis.com/aom-releases/" --output-document index.xml &> /dev/null
-cat index.xml | grep -E -o "libaom-[0-9].[0-9].[0-9].tar.gz" | sort | tail -n 1 | sed 's/libaom-//g' | sed 's/\.tar\.gz//'
-rm -f index.xml
+latest=$(cat index.xml | grep -oP '(?<=<Key>libaom-)[0-9]+\.[0-9]+\.[0-9]+(?=\.tar\.gz)' \
+    | sort -V \
+    | tail -n 1)
+
+echo "$latest"
